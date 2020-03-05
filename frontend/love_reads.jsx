855ce3cdc9e8606e44c898e469309dd4cce995ec
uranussg <uranussg@gmail.com>
2020-03-04 21:23:21 -0800

    remove logger

--- frontend/love_reads.jsx
+++ frontend/love_reads.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from "redux"
-import logger from "redux-logger"
 import thunk from "redux-thunk"
 import rootReducer from './reducers/root_reducer'
 import Root from './components/root'
-import {fetchBooks, fetchBook} from './actions/book_action'
-import {createBookshelf} from './actions/bookshelf_action'
-import {createReview, fetchReviews} from  './actions/review_actions'
+
 
 const configureStore = (preloadState = {}) => 
-  createStore(rootReducer, preloadState, applyMiddleware(thunk, logger))
+  createStore(rootReducer, preloadState, applyMiddleware(thunk))
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -32,12 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     window.getState = store.getState
     window.dispatch = store.dispatch
-    window.fetchBook = fetchBook
-    window.fetchBooks = fetchBooks
-    window.createBookshelf = createBookshelf
 
-    window.fetchReviews = fetchReviews
-    window.createReview = createReview
 
     ReactDOM.render(<Root store={store}/>, root);
 });
