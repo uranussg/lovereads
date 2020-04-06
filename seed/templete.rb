62f9b3bfe01d79358595b48565f95ed0f5a70ebf
uranussg <uranussg@gmail.com>
2020-04-06 16:35:17 -0700

    book description seed

--- seed/templete.rb
+++ seed/templete.rb
@@ -1,6 +1,1 @@
-From fairest creatures we desire increase, That thereby beautys rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feedst thy lights flame with self-substantial fuel,Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the worlds fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And, tender churl, makst waste in niggarding: Pity the world, or else this glutton be, To eat the worlds due, by the grave and thee.
-
-
-
-
-　　Stray birds of summer come to my window to sing and fly away. And yellow leaves of autumn, which have no songs, flutter and fall there with a sign.
+ UNLESS someone like you...cares a whole awful lot...nothing is going to get better...It s not.  Long before saving the earth became a global concern, Dr. Seuss, speaking through his character the Lorax, warned against mindless progress and the danger it posed to the earth s natural beauty. His classic cautionary tale is now available in an irresistible mini-edition, perfect for backpack or briefcase, for Arbor Day, Earth Day, and every day
